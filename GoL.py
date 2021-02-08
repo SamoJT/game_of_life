2eee6e2bcfb8734ed224d3548917141aecd4db98
SamoJT <samojt@gmail.com>
2021-02-08 14:05:50 +0000

    Added function get state of all cells and append to row list

--- GoL.py
+++ GoL.py
@@ -2,9 +2,12 @@ import tkinter as tk
 from random import randrange
 
 def create_grid(w, h, buf):
+    row_and_cols = 0
     for i in range(buf, w, 20):
         canvas.create_line(i, buf, i, h-buf)  # (x0, y0, x1, y1)
         canvas.create_line(buf, i, w-buf, i)
+        row_and_cols += 1
+    print(f'Debug -- Amount of Rows and cols: {row_and_cols}')
     return
 
 def initialise():
@@ -23,8 +26,25 @@ def fill_squares(coords):
         canvas.create_rectangle(x0, y0, x1, y1, fill='red')
     return
 
-def check_squares(w, h, buf, coords):
-    pass      
+def get_state(w, h, buf, coords):
+    rows = [[]for i in range(buf, w-buf, 20)]
+    x0 = buf
+    y0 = buf
+    row = buf
+    while row < 800:
+        index = int((row/2)/10-1)
+        # print(f'Debug -- index: {index}')
+        for i in range(buf, w-buf, 20):
+            x0 = i
+            xy = (x0,y0)
+            # print(f'Debug -- (x0,y0): {xy}')
+            if xy in coords:
+                rows[index].append('Alive')
+            else:
+                rows[index].append('Dead')
+        y0 += 20
+        row = y0
+    print(rows)
 
 root = tk.Tk()
 w = 820
@@ -34,9 +54,9 @@ canvas = tk.Canvas(root, width=w, height=h, background='white')
 
 create_grid(w, h, buf)
 coords = initialise()
-print(f'Debug -- Alive: {len(coords)}')
+# print(f'Debug -- Alive: {len(coords)}')
 fill_squares(coords)
-check_squares(w, h, buf, coords)
+get_state(w, h, buf, coords)
 
 
 
